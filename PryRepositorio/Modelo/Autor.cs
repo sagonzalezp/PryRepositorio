5f696903e2253c70d525e674db9044d15b77b9ba
Sergio Gonzalez Peña <sagonzalezp1@gmail.com>
2018-09-14 23:18:20 -0500

    Creando Autor
    Se crea tabla AUTORES
    Se crea la clase Autor.cs

---
+++ PryRepositorio/Modelo/Autor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PryRepositorio.Modelo
+{
+    public class Autor
+    {
+        String codigo;
+        String nombre;
+
+        public Autor()
+        {
+            this.Codigo = "";
+            this.Nombre = "";
+        }
+
+        public Autor(string codigo, string nombre)
+        {
+            this.Codigo = codigo;
+            this.Nombre = nombre;
+        }
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+}
